1d0eb7395e0d1d6b7f94328169869057f29e83db
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2020-12-30 17:01:52 -0800

    feat(Extension): add more test extension stuff

--- src/extension.ts
+++ src/extension.ts
@@ -4,8 +4,18 @@ export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "toodoo" is now active!');
 
   context.subscriptions.push(
-    vscode.commands.registerCommand("toodoo.helloWorld", () => {
-      vscode.window.showInformationMessage("How was your day?", "good", "bad");
+    vscode.commands.registerCommand("toodoo.helloWorld", async () => {
+      const answer = await vscode.window.showInformationMessage(
+        "How was your day?",
+        "good",
+        "bad"
+      );
+
+      if (answer === "bad") {
+        vscode.window.showInformationMessage("Sorry to hear that!");
+      } else {
+        console.log(answer);
+      }
     })
   );
 }
